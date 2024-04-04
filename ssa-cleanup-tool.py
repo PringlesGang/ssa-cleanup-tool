3487db6399726bc09494fe2cb1796dcdb93f8dd7
PringlesGang <liamelderbroek123@gmail.com>
2024-04-04 22:55:03 +0200

    Refactorization

--- ssa-cleanup-tool.py
+++ ssa-cleanup-tool.py
@@ -3,128 +3,226 @@ import os
 import sys
 import re
 
-from typing import Any, Tuple, List, Set, Pattern
+from typing import Any, Iterable, List, Set, Pattern, NamedTuple
 from pathlib import Path
 
 
-def flattenOnce(iterable: List[List[Any]]) -> List[Any]:
+outputDir = Path("./output")
+
+
+class IOPair(NamedTuple):
+    input: Path
+    output: Path
+
+
+def flattenOnce(iterable: Iterable[Iterable[Any]]) -> Iterable[Any]:
+    """Flatten one level of a multi-level Iterable."""
     return [item for item in [deepIterable for deepIterable in iterable]]
 
 
-def acceptedByWhitelist(line: str, whitelist: Set[Pattern[str]]) -> bool:
-    return True in [(rule.match(line) is not None) for rule in whitelist]
-
-
-def acceptedByBlacklist(line: str, blacklist: Set[Pattern[str]]) -> bool:
-    return True not in [(rule.match(line) is not None) for rule in blacklist]
-
-
 def getFilesInDirectories(directories: List[Path]) -> List[Path]:
+    """Get all files within the provided directories.
+    Not recursive!
+    """
     files: List[Path] = []
-
+    
     for directory in directories:
         files += [file for file in directory.iterdir() if file.is_file()]
-    
+        
     return files
 
 
-def processFilterList(filterPathList: List[str]) -> Set[Pattern[str]]:
-    filterFileList = [Path(path) for path in filterPathList]
+def parseArguments() -> argparse.Namespace:
+    """Parse the command-line arguments passed into the program."""
+    parser = argparse.ArgumentParser(description="Clean up SubStationAlpha files.")
+
+    parser.add_argument("files", type=str, nargs='+',
+                        help="Filepaths to the ssa files, or directories containing them.")
+    parser.add_argument("-d", "--directory", dest="directory", action="store_true",
+                        help="Treat the provided input files as directories containing the input files.")
+    parser.add_argument("-n", "--no-delete", dest="delete", action="store_false",
+                        help="Don't clear out the output directory.")
+    parser.add_argument("-b", "--blacklist", dest="blacklists", type=str, nargs='+', default=[],
+                        help="Filepaths to newline-separated blacklist files.")
+    parser.add_argument("-w", "--whitelist", dest="whitelists", type=str, nargs='+', default=[],
+                        help="Filepaths to newline-separated whitelist files.")
+    parser.add_argument("-o", "--output", dest="outputNames", type=str, nargs='+', default=[],
+                        help="Output names for each given input, in order.")
+
+    return parser.parse_args()
+
+
+def checkArguments(
+    files: List[str],
+    blacklists: Set[str],
+    whitelists: Set[str],
+    outputNames: List[str],
+    directory: bool,
+    delete: bool,
+) -> None:
+    """Check all arguments for validity."""
+    # Check if there is a bijection between the input files and output names
+    if outputNames and len(outputNames) != len(files):
+        print("The amount of output names does not match the amount of input files.")
+        exit(1)
+
+
+def getInputPaths(
+    files: List[str],
+    blacklists: Set[str],
+    whitelists: Set[str],
+    directory: bool,
+) -> List[Path]:
+    """Convert the input files list into a list of Paths, and coalless the directories if necessary."""
+    # Check if all files exist
+    inputPaths = list(map(Path, files))
+    allFiles = map(Path, set(inputPaths).union(blacklists).union(whitelists))
+    if directory:
+        nonexistentDirectory = next((directory for directory in inputPaths if not directory.is_dir()), None)
+        if nonexistentDirectory is not None:
+            print(f"{nonexistentDirectory} is not a directory.", file=sys.stderr)
+            exit(1)
+        inputPaths = getFilesInDirectories(inputPaths)
+    else:
+        nonexistentFile = next((file for file in allFiles if not file.is_file()), None)
+        if nonexistentFile is not None:
+            print(f"{nonexistentFile} is not a file.", file=sys.stderr)
+            exit(1)
     
-    filterSet: set[re.Pattern[str]] = set()
-    for filterFile in filterFileList:
-            fileStream = open(filterFile, 'r')
-            
+    return inputPaths
+
+
+def getIOPairs(
+    files: List[str],
+    blacklists: Set[str],
+    whitelists: Set[str],
+    outputNames: List[str],
+    directory: bool,
+) -> Set[IOPair]:
+    """Pair up the input paths with the corresponding output paths."""
+    inputPaths = getInputPaths(
+        files,
+        blacklists,
+        whitelists,
+        directory
+    )
+    
+    return set(
+        map( # Map to IOPair
+            lambda pair: IOPair(pair[0], pair[1]),
+            zip( # Pair up input files and output files
+                inputPaths,
+                map( # Get output path
+                    lambda name: outputDir.joinpath(name),
+                    # Use output names if provided; otherwise use input names
+                    outputNames if outputNames else list(map(lambda file: Path(file).name, inputPaths)))
+                )
+            )
+        )
+
+
+def acceptedByWhitelist(line: str, whitelist: Set[Pattern[str]]) -> bool:
+    """Check whether the provided line adheres to the whitelist."""
+    return whitelist == set() or next((line for rule in whitelist if rule.match(line)), None) is not None
+
+
+def acceptedByBlacklist(line: str, blacklist: Set[Pattern[str]]) -> bool:
+    """Check whether the provided line adheres to the blacklist."""
+    return next((line for rule in blacklist if rule.match(line)), None) is None
+
+
+def processFilterList(filterPathSet: Set[str]) -> Set[Pattern[str]]:
+    """Convert a list of filter files into a list of RegEx Pattern objects."""
+    filterFileSet = map(Path, filterPathSet)
+    
+    filterSet: Set[Pattern[str]] = set()
+    for filterFile in filterFileSet:
+        with open(filterFile, 'r') as fileStream:
             for line in fileStream:
                 try:
-                    filter = re.compile(line)
+                    filter = re.compile(line.removesuffix('\n'))
                 except re.error:
                     print(f"{line} in {filterFile} is not a valid regular expression; ignoring.", file=sys.stderr)
                 else:
                     filterSet.add(filter)
-            
-            fileStream.close()
-    
+
     return filterSet
 
+
 def processFile(
-    ssaFile: Tuple[Path, Path],
+    ssaFile: IOPair,
     whitelist: Set[Pattern[str]],
     blacklist: Set[Pattern[str]],
 ) -> None:
-    input = ssaFile[0]
-    output = ssaFile[1]
-
-    if output.exists():
-        os.remove(output)
+    """Clean up a single ssa file."""
+    if ssaFile.output.exists():
+        os.remove(ssaFile.output)
     
-    inputStream = open(input, 'r', encoding="UTF-8-sig")
-    outputStream = open(output, 'x')
+    with open(ssaFile.input, 'r') as inputStream:
+        with open(ssaFile.output, 'x') as outputStream:
+            for line in filter(
+                lambda line: acceptedByWhitelist(line, whitelist) and acceptedByBlacklist(line, blacklist),
+                inputStream
+            ):
+                outputStream.write(line)
 
-    for line in inputStream:
-        if acceptedByWhitelist(line, whitelist) and acceptedByBlacklist(line, blacklist):
-            outputStream.write(line)
-        
-    inputStream.close()
-    outputStream.close()
+
+def ssaCleanup(
+    files: List[str],
+    blacklists: Set[str] = set(),
+    whitelists: Set[str] = set(),
+    outputNames: List[str] = [],
+    directory: bool = False,
+    delete: bool = False,
+) -> None:
+    """Clean up SubStationAlpha files by means of black- and whitelists.
+
+    Args:
+        files (List[str]): Filepaths to the ssa files, or directories containing them.
+        blacklist (List[str], optional): Filepaths to newline-separated blacklist files. Defaults to set().
+        whitelist (List[str], optional): Filepaths to newline-separated whitelist files. Defaults to set().
+        outputNames (List[str], optional): Output names for each given input, in order. Defaults to [].
+        directory (bool, optional): Set to True if the `files` argument consists of directories. Defaults to False.
+        delete (bool, optional): Set to True if you want the program to clear the output directory first. Defaults to False.
+    """
+    checkArguments(
+        files,
+        blacklists,
+        whitelists,
+        outputNames,
+        directory,
+        delete
+    )
+    
+    IOPairs = getIOPairs(
+        files,
+        blacklists,
+        whitelists,
+        outputNames,
+        directory
+    )
+    whitelist = processFilterList(whitelists)
+    blacklist = processFilterList(blacklists)
+    
+    # Create the output directory
+    if not outputDir.is_dir():
+        outputDir.mkdir()
+    
+    if delete:
+        for file in [file for file in outputDir.iterdir() if file.is_file()]:
+            os.remove(file)
+    
+    for ssaFile in IOPairs:
+        processFile(ssaFile, whitelist, blacklist)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Clean up SubStationAlpha files.")
-    parser.add_argument("files", type=str, nargs='+',
-                        help="Paths to the files to operate on")
-    parser.add_argument("-d", "--directory", dest="directory", action="store_true",
-                        help="Input paths are directories. All files within will be taken as input.")
-    parser.add_argument("-n", "--no-delete", dest="delete", action="store_false",
-                        help="Don't delete the files currently in the output folder.")
-    parser.add_argument("-b", "--blacklist", dest="blacklists", type=str, nargs='+', default=[],
-                        help="List of newline-separated blacklists describing what RegEx-matched lines to skip.")
-    parser.add_argument("-w", "--whitelist", dest="whitelists", type=str, nargs='+', default=[],
-                        help="List of newline-separated whitelists describing what RegEx-matched lines to keep.")
-    parser.add_argument("-o", "--output", dest="outputNames", type=str, nargs='+', default=[],
-                        help="Output names for each given input.")
-    args = parser.parse_args()
-
-    if args.outputNames and args.outputNames.len() != args.files.len():
-        print("The amount of output names does not match the amount of input files.")
-        exit(1)
-    
-    # Check if all files provided exist
-    allFiles = args.files + args.blacklists + args.whitelists
-    for file in allFiles:
-        if not Path(file).exists():
-            print(f"{file} does not exist.", file=sys.stderr)
-            exit(1)
-    
-    inputPaths = [Path(file) for file in args.files]
-    whitelist = processFilterList(args.whitelists)
-    blacklist = processFilterList(args.blacklists)
-    
-    if args.directory:
-        for directory in inputPaths:
-            if not Path(directory).is_dir():
-                print(f"{directory} is not a directory.", file=sys.stderr)
-                exit(1)
-        inputPaths = getFilesInDirectories(inputPaths)
-    else:
-        for file in inputPaths:
-            if not Path(file).is_file():
-                print(f"{file} is not a file.", file=sys.stderr)
-                exit(1)
-    
-    # If no output names are given, use the names of the input
-    outputNames = args.outputNames if args.outputNames else [Path(file).name for file in inputPaths]
-    outputPaths = [Path("./output").joinpath(name) for name in outputNames]
-
-    processFiles = zip(inputPaths, outputPaths)
-
-    # Create the output directory
-    if not Path("./output").is_dir():
-        Path("./output").mkdir()
-    
-    if args.delete:
-        for file in [file for file in Path("./output").iterdir() if file.is_file()]:
-            os.remove(file)
-    
-    for ssaFile in processFiles:
-        processFile(ssaFile, whitelist, blacklist)
+    args = parseArguments()
+    ssaCleanup(
+        args.files,
+        args.blacklists,
+        args.whitelists,
+        args.outputNames,
+        args.directory,
+        args.delete,
+    )
